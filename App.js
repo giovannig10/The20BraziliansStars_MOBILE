a3c22c27b7e8a3a75cee85a3b6244bd7f4161c4c
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-03-13 11:40:25 -0300

    feat: Start to create the Log-in page

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{ uri: './img/LogoDark'}}/>
+      <Text>The 20 brazilian Stars</Text>
       <StatusBar style="auto" />
+
+    <View style={styles.inputs}>
+      <TextInput style={styles.text} placeholder='Email:'></TextInput>
+      <TextInput style={styles.text} placeholder='Senha:'></TextInput>
+    </View>
+    <View>
+      <Button title='Entrar' color='#B7B8BA'></Button>
+
+    </View>
     </View>
   );
 }
@@ -17,4 +27,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  inputs: {
+    display: 'flex',
+    alignItems: 'left',
+
+     
+  },
+
+  text: {
+    color: '#9A9999',
+    backgroundColor: '#B7B8BA',
+    borderRadius: 5,
+    width: 348,
+    height: 33,
+    display: 'flex',
+    textAlign: 'left',
+    margin: 7,
+  },
+
 });
