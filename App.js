3ae6e33a218e9d5571d0a08322f0245ee300212c
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-03-13 11:55:26 -0300

    feat: add log-in button

--- App.js
+++ App.js
@@ -5,16 +5,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Image source={{ uri: './img/LogoDark'}}/>
+      <View>      </View>
       <Text>The 20 brazilian Stars</Text>
       <StatusBar style="auto" />
+
 
     <View style={styles.inputs}>
       <TextInput style={styles.text} placeholder='Email:'></TextInput>
       <TextInput style={styles.text} placeholder='Senha:'></TextInput>
     </View>
     <View>
-      <Button title='Entrar' color='#B7B8BA'></Button>
-
+      <Button style={styles.botao} title='Entrar' color='#25406A'></Button>
+      <Text>Esqueceu a senha?</Text>
     </View>
     </View>
   );
@@ -23,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D0D0D0',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -44,6 +46,14 @@ const styles = StyleSheet.create({
     display: 'flex',
     textAlign: 'left',
     margin: 7,
+    paddingLeft: 4,
+    fontSize: 25
   },
+
+  botao: {
+    width: 171,
+    height: 39,
+    color: '#25406A',
+  }
 
 });
